76c1bfa8b1d27be09a296a8c1d26bc453318a166
Woo <rhrhghrhrhgh@ajou.ac.kr>
2025-01-16 13:27:24 +0900

    add @JsonProperty

--- src/main/java/io/goorm/mybatis/board/model/Board.java
+++ src/main/java/io/goorm/mybatis/board/model/Board.java
@@ -1,7 +1,10 @@
 package io.goorm.mybatis.board.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Setter
 @Getter
@@ -12,5 +15,10 @@ public class Board {
     private String title;
 
     private String contents;
-    
+
+    @JsonProperty("reg_user")
+    private String regUser;
+
+    @JsonProperty("reg_date")
+    private LocalDateTime regDate;
 }
